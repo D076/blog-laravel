561f27c85a08324408a3397529d0da9a7351e85d
Dmitry Titov <piratik44@mail.ru>
2022-09-13 17:34:08 +0700

    Create personal area views

---
+++ resources/views/personal/comment/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('personal.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Комментарий</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{ route('personal.comment.update', $comment) }}" method="POST" class="w-50">
+                            @csrf
+                            @method('PATCH')
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" cols="30" rows="10">{{$comment->message}}</textarea>
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Обновить">
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <!-- Main row -->
+
+                <!-- /.row (main row) -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
