7c612022f561814d705b9220dcc5d56f63c7dbe3
Dmitry Titov <piratik44@mail.ru>
2022-09-13 17:35:17 +0700

    Update controllers

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index');
+        # Все посты по 4 штуки - сначала новые
+        $posts = Post::orderBy('updated_at', 'desc')->paginate(4);
+        # Три случайных поста - сначала новые
+        $randomPosts = Post::orderBy('updated_at', 'desc')->get()->random(3);
+        # Четыре самых пролайканных поста
+        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'desc')->get()->take(4);
+        return view('main.index', compact('posts', 'randomPosts', 'likedPosts'));
     }
 }
